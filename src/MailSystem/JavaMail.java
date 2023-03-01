37fed303f9a3fbe006541add3d34869fd4ab0047
G.V.Ishan Ravidhu Dilshan <123403611+GVIRDilshan@users.noreply.github.com>
2023-03-01 15:22:56 +0530

    New change

--- src/MailSystem/JavaMail.java
+++ src/MailSystem/JavaMail.java
@@ -8,8 +8,8 @@ import java.util.Properties;
 
 public class JavaMail {
     public static void sendMail(String recepient) throws Exception {
-        System.out.println("Preparing to send email");
-
+        System.out.println("Preparing to send email 1");
+        System.out.println("Preparing to send email 2");
     }
 }
 
